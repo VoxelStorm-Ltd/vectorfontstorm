c45461308bab8337c586a2fb053108a1e7110732
SlowRiot <rain.backnet@gmail.com>
2015-09-21 05:34:03 +0100

    including all relevant components in the main header

--- vectorfontstorm/vectorfontstorm.h
+++ vectorfontstorm/vectorfontstorm.h
@@ -2,6 +2,11 @@
 #define VECTORFONTSTORM_H_INCLUDED
 
 #include "manager.h"
+#include "point.h"
+#include "segment.h"
+#include "contour.h"
+#include "glyph.h"
+#include "font.h"
 #include "string.h"
 
 #endif // VECTORFONTSTORM_H_INCLUDED
