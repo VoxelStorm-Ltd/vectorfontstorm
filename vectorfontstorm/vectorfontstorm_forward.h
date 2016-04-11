acc3ca85c0031a18d882c0654337d9e69ade0988
SlowRiot <slowriot@voxelstorm.com>
2016-04-11 22:11:36 +0100

    adding forward declaration headers

---
+++ vectorfontstorm/vectorfontstorm_forward.h
@@ -0,0 +1,17 @@
+#ifndef VECTORFONTSTORM_FORWARD_H_INCLUDED
+#define VECTORFONTSTORM_FORWARD_H_INCLUDED
+
+namespace vectorfontstorm {
+
+class point;
+template<typename T> struct buffer_data;
+template<typename T> class manager;
+template<typename T> class segment;
+template<typename T> class contour;
+template<typename T> class glyph;
+template<typename T> class font;
+template<typename T> class string;
+
+}
+
+#endif // VECTORFONTSTORM_FORWARD_H_INCLUDED
