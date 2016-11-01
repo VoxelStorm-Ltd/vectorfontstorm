9a9ad854eb29663e113a57acac7120199a85df1a
SlowRiot <slowriot@voxelstorm.com>
2016-11-01 04:47:19 +0000

    typename not class in template declaration

--- vectorfontstorm/vectorfontstorm_forward.h
+++ vectorfontstorm/vectorfontstorm_forward.h
@@ -3,13 +3,15 @@
 
 namespace vectorfontstorm {
 
-class point;
+enum class aligntype;
+template<typename T> struct buffer;
 template<typename T> struct buffer_data;
-template<typename T> class manager;
-template<typename T> class segment;
 template<typename T> class contour;
-template<typename T> class glyph;
 template<typename T> class font;
+template<typename T> class glyph;
+template<typename T> class manager;
+class point;
+template<typename T> class segment;
 template<typename T> class string;
 
 }
